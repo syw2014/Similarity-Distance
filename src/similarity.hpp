79b01817914253cde808d90effd8b5ccaa595309
JerryShi <jerryshi0110@hotmail.com>
2015-08-16 14:58:17 +0800

    add source file

---
+++ src/similarity.hpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+   @ File Name: similarity.hpp
+   @ Method: 
+   @ Author: Jerry Shi
+   @ Mail: jerryshi0110@gmail.com 
+   @ Created Time: 2015年08月16日 星期日 14时38分56秒
+ ************************************************************************/
+
+#include <iostream>
+
+class similarity
+{
+	public:
+		similarity();
+		~similarity();
+
+		void editDistance(std::string str1,std::string str2);
+};
